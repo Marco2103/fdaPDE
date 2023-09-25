52a55dd2564afbcf4be28eff2541864926a58584
Marco Francesco De Sanctis <marcofrancesco99@gmail.com>
2023-09-25 15:03:41 +0000

    .gitignore removed, added mesh files and other ignored contents

---
+++ test/CMakeFiles/fdaPDE_test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fdaPDE_test.
+# This may be replaced when dependencies are built.
---
+++ test/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/marco/PACS/Project/Code/Cpp/fdaPDE-fork/test/_deps/googletest-src
+# Build directory: /mnt/c/Users/marco/PACS/Project/Code/Cpp/fdaPDE-fork/test/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
